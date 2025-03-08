97052edcb7a33ff7e1b6c060326a61008d46e63a
Emmanuel kipchirchir <130069395+Ekipchirchir@users.noreply.github.com>
2025-03-08 19:53:15 +0300

    Delete backend directory

--- backend/backend/InstanTransfer/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UserAccount, Wallet,Transaction, Currency
-
-# Register your models here.
-admin.site.register(UserAccount)
-admin.site.register(Wallet)
-admin.site.register(Transaction)
-admin.site.register(Currency)
