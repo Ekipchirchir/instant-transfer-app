91f5a53b9b7967d1762b47fd513bc2a068eaa42b
Emmanuel kipchirchir <130069395+Ekipchirchir@users.noreply.github.com>
2025-04-13 12:02:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
+import { Platform } from "react-native";
 const API_BASE_URL = "https://instant-transfer-back-production.up.railway.app/api";  
+Platform.OS === "android"
+    ? "http://192.168.0.164:8000/api" 
+    : "http://127.0.0.1:8081/api";  
 
 export default API_BASE_URL;
